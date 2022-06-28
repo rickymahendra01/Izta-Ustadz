49cdcd3881aa22352a87008ac3a017abaacb7f5f
Ricky Mahendra <rickymahendra465@gmai.com>
2022-06-28 16:09:23 +0800

    gambar pada responsif

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -54,7 +54,7 @@
     <!-- Navbar Start -->
     <nav class="navbar navbar-expand-lg bg-primary navbar-dark shadow-sm py-3 py-lg-0 px-3 px-lg-5">
         <a href="index.html" class="navbar-brand d-flex d-lg-none">
-            <h1 class="m-0 display-4 text-secondary"><span class="text-white">Farm</span>Fresh</h1>
+            <img class="img-fluid" width="256px" height="144px" src="{{asset('user/img/logo.png')}}" alt="">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <span class="navbar-toggler-icon"></span>
